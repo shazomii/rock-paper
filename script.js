6945a07533ce95c2d66b758f74ce537417290a78
shazomii <seyeoyeniran2u@gmail.com>
2025-04-22 14:17:48 +0100

    Add logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    switch (computerChoice) {
+        case 1:
+            return "ROCK";
+        case 2:
+            return "PAPER";
+        default:
+            return "SCISSORS";
+    }
+}
+
+console.log(getComputerChoice());
